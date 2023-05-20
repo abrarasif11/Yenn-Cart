369d60d90dfb2f9a66763cb5bf259df94feb1b67
abrarasif11 <faasif929@gmail.com>
2023-05-20 20:21:44 +0600

    right-site-left-side-complete

--- components/ProductDetailsCarousel.jsx
+++ components/ProductDetailsCarousel.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-
-
+import Image from 'next/image';
+import img1 from '../public/p1.png';
+import img2 from '../public/p2.png';
+import img3 from '../public/p3.png';
+import img4 from '../public/p4.png';
+import img5 from '../public/p5.png';
+import img6 from '../public/p6.png';
+import img7 from '../public/p7.png';
 const ProductDetailsCarousel = () => {
     return (
         <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
@@ -13,8 +19,13 @@ const ProductDetailsCarousel = () => {
                 thumbWidth={60}
                 className="productCarousel"
             >
-               
-
+                 <Image src={img1} />
+                 <Image src={img2} />
+                 <Image src={img3} />
+                 <Image src={img4} />
+                 <Image src={img5} />
+                 <Image src={img6} />
+                 <Image src={img7} />
             </Carousel>
         </div>
     )
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,7 +12,131 @@ const ProductDetails = () => {
                         <ProductDetailsCarousel></ProductDetailsCarousel>
                     </div>
                     {/* Right col start */}
-                    <div className="flex-[1] py-3">Right</div>
+                    <div className="flex-[1] py-3">
+                        {/* PRODUCT TITLE */}
+                        <div className="text-[34px] font-semibold mb-2 leading-tight">
+                            Jordan Retro 6 G
+                        </div>
+
+                        {/* PRODUCT SUBTITLE */}
+                        <div className="text-lg font-semibold mb-5">
+                            Men&apos;s Golf Shoes
+                        </div>
+
+                        {/* PRODUCT PRICE */}
+                        <div className="flex items-center">
+                            <p className="mr-2 text-lg font-semibold">
+                                MRP : $15.00
+                            </p>
+                            {/* {p.original_price && (
+                                <>
+                                    <p className="text-base  font-medium line-through">
+                                        &#8377;{p.original_price}
+                                    </p>
+                                    <p className="ml-auto text-base font-medium text-green-500">
+                                        {getDiscountedPricePercentage(
+                                            p.original_price,
+                                            p.price
+                                        )}
+                                        % off
+                                    </p>
+                                </>
+                            )} */}
+                        </div>
+
+                        <div className="text-md font-medium text-black/[0.5]">
+                            incl. of taxes
+                        </div>
+                        <div className="text-md font-medium text-black/[0.5] mb-20">
+                            {`(Also includes all applicable duties)`}
+                        </div>
+                        {/* PRODUCT SIZE RANGE START */}
+                        <div className="mb-10">
+                            {/* HEADING START */}
+                            <div className="flex justify-between mb-2">
+                                <div className="text-md font-semibold">
+                                    Select Size
+                                </div>
+                                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                                    Select Guide
+                                </div>
+                            </div>
+                            {/* size start */}
+                            <div className='grid grid-cols-3 gap-2'>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer'>
+                                    UK 6
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer'>
+                                    UK 9
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer'>
+                                    UK 12
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer'>
+                                    UK 15
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer'>
+                                    UK 18
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-not-allowed opacity-50'>
+                                    UK 21
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-not-allowed opacity-50'>
+                                    UK 24
+                                </div>
+                                <div className='border rounded-md text-center py-3 font-medium hover:border-black cursor-not-allowed opacity-50'>
+                                    UK 27
+                                </div>
+                            </div>
+                            {/* size end */}
+                        </div>
+                        {/* Show error start */}
+                        <div className='text-red-600 mt-1 mb-5'>
+                            Size selection is required
+                        </div>
+                        {/* Show error end */}
+
+                        {/* SIZE Range END */}
+
+                        {/* ADD TO CART BUTTON START */}
+                        <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
+                            Add to Cart
+                        </button>
+                        {/* ADD TO CART BUTTON END */}
+
+                        {/* WHISHLIST BUTTON START */}
+                        <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+                            Whishlist
+                            <IoMdHeartEmpty size={20} />
+                        </button>
+                        {/* WHISHLIST BUTTON END */}
+
+                        <div>
+                            <div className="text-lg font-bold mb-5">
+                                Product Details
+                            </div>
+                            <div className="markdown text-md mb-5">
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                            </div>
+                            <div className="markdown text-md mb-5">
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                                Feel unbeatable from the box.
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Wrapper>
         </div>
