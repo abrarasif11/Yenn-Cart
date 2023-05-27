83474fabd6c833b41a1c05c78e53ac822a5c7468
abrarasif11 <faasif929@gmail.com>
2023-05-27 11:16:52 +0600

    data-load-except-image

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -3,14 +3,15 @@ import Link from 'next/link'
 import React from 'react'
 import shoe from '../components/assests/product-1.webp'
 
-const ProductCard = () => {
+const ProductCard = ({data: {attributes: p, id}}) => {
   return (
-    <Link href='/product/1' className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
-       <Image className='w-full' src={shoe}/>
+    <Link href={`/products/${p.slug}`} 
+    className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+       {/* <Image className='w-full'  src={p.thumbnail.data.attributes.url}/> */}
        <div className='p-4 text-black'>
-         <h2 className="text-lg font-medium">Product Name</h2>
+         <h2 className="text-lg font-medium">{p.name}</h2>
          <div  className="flex items-center text-black">
-            <p className="mr-2 text-lg font-semibold">$20.00</p>
+            <p className="mr-2 text-lg font-semibold">{p.price}</p>
             <p className="text-base  font-medium line-through">$25.00</p>
             <p className="ml-auto text-base font-medium text-green-500">20% off</p>
          </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <main >
-      <HeroBanner></HeroBanner>
+      <HeroBanner>
+
+      </HeroBanner>
       <Wrapper>
         <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
@@ -18,20 +21,30 @@ export default function Home() {
           </div>
         </div>
         {/*Star Show Card */}
-        <div  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))}
+          {/* <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard /> */}
         </div>
         {/* End Show Card */}
       </Wrapper>
     </main>
   );
 
+}
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
