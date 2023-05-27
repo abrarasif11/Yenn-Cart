8190a4e306b5e3f0e90b3cf40c38e6c881cf3b6b
abrarasif11 <faasif929@gmail.com>
2023-05-27 18:00:10 +0600

    added-discount

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -2,20 +2,39 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import shoe from '../components/assests/product-1.webp'
+import { getDiscountedPricePercentage } from '@/utils/helper'
 
-const ProductCard = ({data: {attributes: p, id}}) => {
+const ProductCard = ({ data: { attributes: p, id } }) => {
   return (
-    <Link href={`/products/${p.slug}`} 
-    className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
-       {/* <Image className='w-full'  src={p.thumbnail.data.attributes.url}/> */}
-       <div className='p-4 text-black'>
-         <h2 className="text-lg font-medium">{p.name}</h2>
-         <div  className="flex items-center text-black">
-            <p className="mr-2 text-lg font-semibold">{p.price}</p>
-            <p className="text-base  font-medium line-through">$25.00</p>
-            <p className="ml-auto text-base font-medium text-green-500">20% off</p>
-         </div>
-       </div>
+    <Link href={`/products/${p.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data.attributes.url}
+        alt={p.name}
+      />
+      <div className='p-4 text-black'>
+        <h2 className="text-lg font-medium">{p.name}</h2>
+        <div className="flex items-center text-black">
+          <p className="mr-2 text-lg font-semibold">{p.price} Tk</p>
+
+          {p.original_price && (
+            <>
+              <p className="text-base  font-medium line-through">
+                {p.original_price} Tk
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {getDiscountedPricePercentage(
+                  p.original_price,
+                  p.price
+                )}
+                % off
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </Link>
   )
 }
