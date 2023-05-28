cf1b421ece444a628255dfa3dcf5a31db6b5cf5f
abrarasif11 <faasif929@gmail.com>
2023-05-29 05:53:10 +0600

    navbar-category

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,11 +9,13 @@ import { BsCart } from "react-icons/bs";
 import { BiMenuAltRight } from "react-icons/bi";
 import { VscChromeClose } from "react-icons/vsc";
 import Image from "next/image";
+import { fetchDataFromApi } from "@/utils/api";
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState("translate-y-0");
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [categories, setCategories] = useState(null);
 
   const controlNavbar = () => {
     if (window.scrollY > 200) {
@@ -36,6 +38,15 @@ const Navbar = () => {
       window.removeEventListener("scroll", controlNavbar)
     }
   }, [lastScrollY])
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    const { data } = await fetchDataFromApi("/api/categories?populate=*");
+    setCategories(data);
+  };
   return (
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
@@ -44,8 +55,11 @@ const Navbar = () => {
         <Link href="/">
           <Image src={nav} className="w-[30px] md:w-[50px]" />
         </Link>
-        <Menu showCatMenu={showCatMenu}
-          setShowCatMenu={setShowCatMenu}>
+        <Menu
+          showCatMenu={showCatMenu}
+          setShowCatMenu={setShowCatMenu}
+          categories={categories}
+        >
         </Menu>
 
         {
@@ -54,6 +68,7 @@ const Navbar = () => {
             showCatMenu={showCatMenu}
             setShowCatMenu={setShowCatMenu}
             setMobileMenu={setMobileMenu}
+            categories={categories}
           >
           </MenuMobile>
         }
