c40bd43e8e9debfbb16a0c94de3ce4d781842cfb
abrarasif11 <faasif929@gmail.com>
2023-06-12 22:26:44 +0600

    cart-size-quantity-fix

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,14 +1,27 @@
+import { updateCart } from '@/store/cartSlice';
 import Image from 'next/image';
 import React from 'react'
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { useDispatch } from 'react-redux';
 // import img1 from '../components/assests/product-1.webp'
 const CartItem = ({ data }) => {
     const p = data.attributes;
+    
+    const dispatch = useDispatch();
+
+    const updateCartItem = (e, key) => {
+    let payload = {
+        key,
+        val: key === "quantity" ? parseInt(e.target.value) : e.target.value,
+        id: data.id,
+    };
+    dispatch(updateCart(payload))
+    }
     return (
         <div className='flex py-5 gap-3 md:gap-5 border-b'>
             {/* Image Start */}
             <div className='shrink-0 aspect-square w-[50px] md:w-[120px]'>
-                <Image 
+                <Image
                     src={p.thumbnail.data.attributes.url}
                     alt={p.name}
                     width={120}
@@ -40,30 +53,52 @@ const CartItem = ({ data }) => {
                 <div className="flex items-center justify-between mt-4">
                     <div className="flex items-center gap-2 md:gap-10 text-black/[0.5] text-sm md:text-md">
                         <div className="flex items-center gap-1">
-                            <div className="font-semibold">Size:</div>
+                        <div className="font-semibold">Size:</div>
                             <select
-                                className="hover:text-black">
-                                <option value="1">UK 6</option>
-                                <option value="2">UK 9</option>
-                                <option value="3">UK 12</option>
-                                <option value="4">UK 15</option>
-                                <option value="5">UK 18</option>
-                                <option value="6">UK 21</option>
-                                <option value="7">UK 24</option>
+                                className="hover:text-black"
+                                onChange={(e) =>
+                                    updateCartItem(e, "selectedSize")
+                                }
+                            >
+                                {p.size.data.map((item, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={item.size}
+                                            disabled={
+                                                !item.enabled ? true : false
+                                            }
+                                            selected={
+                                                data.selectedSize === item.size
+                                            }
+                                        >
+                                            {item.size}
+                                        </option>
+                                    );
+                                })}
                             </select>
                         </div>
 
                         <div className="flex items-center gap-1">
                             <div className="font-semibold">Quantity:</div>
                             <select
-                                className="hover:text-black">
-                                <option value="1">1</option>
-                                <option value="1">2</option>
-                                <option value="1">3</option>
-                                <option value="1">4</option>
-                                <option value="1">5</option>
-                                <option value="1">6</option>
-                                <option value="1">7</option>
+                                className="hover:text-black"
+                                onChange={(e) =>
+                                    updateCartItem(e, "quantity")
+                                }
+                                >
+                                {Array.from({ length: 10 }, (_, i) => i + 1).map((q, i) => {
+                                    return (
+                                        <option
+                                            key={i}
+                                            value={q}
+                                            selected={data.quantity === q}
+                                        >
+                                            {q}
+                                        </option>
+
+                                    )
+                                })}
                             </select>
                         </div>
                     </div>
